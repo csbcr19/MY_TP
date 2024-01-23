6aa4608bf8971d4b706950deb2036303e02e23f3
eunji <jynn7nj9@naver.com>
2024-01-23 13:47:40 +0900

    Feat: 위치, 컨트롤 생성

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(37.45639, 126.70528), // 지도의 중심좌표
-        level: 4 // 지도의 확대 레벨
+        level: 3 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성
@@ -15,3 +15,14 @@ var marker = new kakao.maps.Marker({
 });
 
 marker.setMap(map);
+
+//지도 컨트롤러 타입 생성
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 줌 컨트롤 생성
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
