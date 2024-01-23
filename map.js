6204dd1e1b6dd4f4c712a7f9300d7e6adabf2d2d
eunji <jynn7nj9@naver.com>
2024-01-23 13:33:06 +0900

    Feat: 위치 변경

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(37.45639, 126.70528), // 지도의 중심좌표
-        level: 2 // 지도의 확대 레벨
+        level: 4 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성
