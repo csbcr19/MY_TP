846ed9a66416dcda6d31ded822e0c97d79e20c9c
eunji <jynn7nj9@naver.com>
2024-01-23 10:15:02 +0900

    Feat: 지도 내용 수정

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성
 
 // 마커가 표시될 위치
-var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);
+var markerPosition  = new kakao.maps.LatLng(37.45639, 126.70528);
 
 // 마커를 생성
 var marker = new kakao.maps.Marker({
